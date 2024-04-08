81ba06a709dacc5bb88a30434d715de2de8b3aa7
chen <chen@qq.com>
2024-04-08 23:23:36 +0800

    提交远程仓库 ————> master

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ public class test {
 
 
     public static void main(String[] args) {
-        System.out.println("新建本地仓库 ————> chen");
-        System.out.println("提交远程仓库 ————> chen");
+        System.out.println("提交远程仓库 ————> master");
+
     }
 }
